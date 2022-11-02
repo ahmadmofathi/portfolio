873e51236985d32a118111e5339013f456e5860c
Ahmad Fathy <90341142+ahmadmofathi@users.noreply.github.com>
2022-11-02 23:10:57 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Landing typing effect
 var type = ["Full Stack Web Developer"];
 var textPosition=0;
 var speed =100;
@@ -10,3 +11,50 @@ typewriter =()=>{
 }
 
 window.addEventListener("load",typewriter);
+
+
+// Progress bars effect
+let section = document.querySelector(".skills .container");
+let progress = document.querySelectorAll(".progress-bar");
+
+//active section effect
+let PageSections = document.querySelectorAll(".page")
+let tabs = document.querySelectorAll(".nav-link");
+
+window.onscroll=function(){
+
+    if(window.scrollY>=section.offsetTop-150){
+        progress.forEach((prog) =>{
+            prog.style.width = prog.dataset.width;
+        })
+    }
+
+
+    if(window.scrollY>=PageSections[0].offsetTop -400 && window.scrollY<PageSections[1].offsetTop -300 ){
+        tabs[0].classList.add("active");
+        tabs[1].classList.remove("active");
+        tabs[3].classList.remove("active");
+        tabs[2].classList.remove('active');
+
+    }
+    else if(window.scrollY>=PageSections[1].offsetTop -400 && window.scrollY<PageSections[2].offsetTop -300 ){
+        tabs[0].classList.remove("active");
+        tabs[2].classList.remove('active');
+        tabs[3].classList.remove("active");
+        tabs[1].classList.add("active");
+    }
+    else if(window.scrollY>=PageSections[2].offsetTop -400 && window.scrollY<PageSections[3].offsetTop -300){
+        tabs[1].classList.remove('active');
+        tabs[2].classList.add("active");
+        tabs[3].classList.remove("active");
+        tabs[0].classList.remove('active');
+    }
+    else if(window.scrollY>=PageSections[3].offsetTop -400 && window.scrollY<PageSections[4].offsetTop -300){
+        tabs[1].classList.remove('active');
+        tabs[2].classList.remove("active");
+        tabs[3].classList.add("active");
+        tabs[0].classList.remove('active');
+    }
+}
+
+//scroll to view 
